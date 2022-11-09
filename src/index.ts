3a8f0e224962c9f4a385707f7f6626626174a918
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2022-11-09 10:52:09 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * as skyline from "./skyline";
 export * as Messages from "./messages";
 export * as Responses from "./responses";
-export * as Progress from "./progress";
+export {Progress} from "./progress";
 export { BasicMessenger, BackendSupplier, SwitchBackend, DefaultMessenger } from "./backend";
